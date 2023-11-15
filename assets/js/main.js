d696e28adde6662cde8f1263191aaec8d3dc021c
scott fennell <scottfennell@scotts-MacBook-Pro-2.local>
2023-11-14 19:41:54 -0500

    innit

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,25 +20,6 @@ $(function() {
         }
     });
     
-    
-    //===== Section Menu Active
-
-    var scrollLink = $('.page-scroll');
-    // Active link switching
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
-
-        scrollLink.each(function () {
-
-            var sectionOffset = $(this.hash).offset().top - 73;
-
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-            }
-        });
-    });
-    
     //===== close navbar-collapse when a  clicked
 
     $(".navbar-nav a").on('click', function () {
